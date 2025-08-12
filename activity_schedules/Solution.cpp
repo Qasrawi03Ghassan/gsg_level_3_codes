e2504838ee534f8221c15b1d81f444484296db0d
Ghassan M. Qasrawi <159701545+Qasrawi03Ghassan@users.noreply.github.com>
2025-08-12 18:31:40 +0300

    Add files via upload

---
+++ activity_schedules/Solution.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <vector>
+#include <string>
+using namespace std;
+
+vector<string> acts = { "Swimming", "Running", "Football" };
+int totalCount = 0;
+
+void R(int day, int n, string lastActivity, vector<string> schedule) {
+    if (day == n) {
+        for (int i = 0; i < n; i++) {
+            cout << schedule[i] << (i == n - 1 ? '\n' : ' ');
+        }
+        totalCount++;
+        return;
+    }
+
+    for (string activity : acts) {
+        if (activity != lastActivity) {
+            schedule[day] = activity;
+            R(day + 1, n, activity, schedule);
+        }
+    }
+}
+
+int main() {
+    int n;
+    cin >> n;
+
+    vector<string> schedule(n);
+    R(0, n, "", schedule);
+
+    cout << endl <<"Count: " << totalCount << endl;
+    return 0;
+}
